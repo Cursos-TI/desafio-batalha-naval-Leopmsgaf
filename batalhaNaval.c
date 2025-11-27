57b4e17598793aa0f0120a8702415ec455a02065
Leopmsgaf <leofjpmsgaf@gmail.com>
2025-11-27 18:35:37 +0000

    criar notas de alunos

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,23 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+   int index;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    char * NomesAlunos [3] [3] = {
+        {"Aluno 0", "Pt: 30", "Mt: 90"},
+        {"Aluno 1", "Pt: 60", "Mt: 60"},
+        {"Aluno 2", "Pt: 90", "Mt: 30"}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    };
+    printf("Digite o numero do Aluno que queira ver a Nota... \n");
+    printf("Para o Aluno 1, digite 0 \n");
+    printf("para o Aluno 2, digite 1 \n");
+    printf("para o Aluno 3, digite 2 \n");
+  scanf("%d", &index);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+  printf("A nota do %s são: %s, %s... \n", NomesAlunos [index][0], NomesAlunos[index][1],NomesAlunos[index][2]);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
 
     return 0;
 }
